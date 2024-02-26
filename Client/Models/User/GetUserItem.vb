6533d19c872ba2d45b4af247bdb8b998c1c1dfb8
Eray Türemiş <85448685+eraytremi@users.noreply.github.com>
2024-02-26 14:41:06 +0300

    .

--- Client/Models/User/GetUserItem.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class GetUserItem
-    Public Property Id As Long
-    Public Property FirstName As String
-    Public Property LastName As String
-    Public Property Email As String
-    Public Property Token As String
-
-End Class
--- Client/Models/User/LoginVm.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class LoginVm
-    Public Property email As String
-    Public Property password As String
-
-
-End Class
